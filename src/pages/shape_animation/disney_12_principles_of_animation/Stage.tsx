f9c129279f332dfd574d9f000693d390cb838449
RuiOkazaki <okazaki200226@gmail.com>
2022-03-19 14:15:28 +0900

    feat: ボタンで切り替えれる

--- src/pages/shape_animation/disney_12_principles_of_animation/Stage.tsx
+++ src/pages/shape_animation/disney_12_principles_of_animation/Stage.tsx
@@ -1,4 +1,16 @@
 import { useState, VFC } from 'react';
+import Anticipation from './animations/Anticipation';
+import Appeal from './animations/Appeal';
+import Arc from './animations/Arc';
+import Exaggeration from './animations/Exaggeration';
+import Follow_through_and_overlapping_action from './animations/Follow_through_and_overlapping_action';
+import Secondary_action from './animations/Secondary_action';
+import Slow_in_and_slow_out from './animations/Slow_in_and_slow_out';
+import Solid_drawing from './animations/Solid_drawing';
+import Squash_and_stretch from './animations/Squash_and_stretch';
+import Staging from './animations/Staging';
+import Straight_ahead_action_and_pose_to_pose from './animations/Straight_ahead_action_and_pose_to_pose';
+import Timing from './animations/Timing';
 import s from './index.module.scss';
 
 type Props = {
@@ -41,8 +53,8 @@ const titlesJP = [
 ];
 
 const Stage: VFC = () => {
-  const [displayAnimation, setDisplayAnimation] = useState('default');
-  const clickHandler = (aniName: string) => setDisplayAnimation(aniName);
+  const [dis, setDis] = useState('default');
+  const clickHandler = (aniName: string) => setDis(aniName);
 
   return (
     <>
@@ -56,7 +68,24 @@ const Stage: VFC = () => {
           />
         ))}
       </div>
-      <div className={s.stage}>{displayAnimation}</div>
+      <div className={s.stage}>
+        {dis === 'Squash_and_stretch' && <Squash_and_stretch />}
+        {dis === 'Anticipation' && <Anticipation />}
+        {dis === 'Staging' && <Staging />}
+        {dis === 'Straight_ahead_action_and_pose_to_pose' && (
+          <Straight_ahead_action_and_pose_to_pose />
+        )}
+        {dis === 'Follow_through_and_overlapping_action' && (
+          <Follow_through_and_overlapping_action />
+        )}
+        {dis === 'Slow_in_and_slow_out' && <Slow_in_and_slow_out />}
+        {dis === 'Arc' && <Arc />}
+        {dis === 'Secondary_action' && <Secondary_action />}
+        {dis === 'Timing' && <Timing />}
+        {dis === 'Exaggeration' && <Exaggeration />}
+        {dis === 'Solid_drawing' && <Solid_drawing />}
+        {dis === 'Appeal' && <Appeal />}
+      </div>
     </>
   );
 };
